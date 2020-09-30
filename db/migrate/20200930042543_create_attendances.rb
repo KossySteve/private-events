497dc9805e4aeee72900f44f926df92ddb2c4396
mimi peshy <mimipeshy@gmail.com>
2020-09-30 19:12:14 +0300

    add attended events db associations

--- db/migrate/20200930042543_create_attendances.rb
+++ db/migrate/20200930042543_create_attendances.rb
@@ -1,9 +1,9 @@
 class CreateAttendances < ActiveRecord::Migration[6.0]
   def change
     create_table :attendances do |t|
-      t.integer :attended_event_id
-      t.integer :attendee_id
-      
+      t.belongs_to :attendee, foreign_key: { to_table: :users }
+      t.belongs_to :attended_event, foreign_key: { to_table: :events }
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,12 @@
 ActiveRecord::Schema.define(version: 2020_09_30_042543) do
 
   create_table "attendances", force: :cascade do |t|
-    t.integer "attended_event_id"
     t.integer "attendee_id"
+    t.integer "attended_event_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["attended_event_id"], name: "index_attendances_on_attended_event_id"
+    t.index ["attendee_id"], name: "index_attendances_on_attendee_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -30,11 +32,11 @@ ActiveRecord::Schema.define(version: 2020_09_30_042543) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "creator_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["creator_id"], name: "index_users_on_creator_id"
   end
 
+  add_foreign_key "attendances", "events", column: "attended_event_id"
+  add_foreign_key "attendances", "users", column: "attendee_id"
   add_foreign_key "events", "users", column: "creator_id"
 end
