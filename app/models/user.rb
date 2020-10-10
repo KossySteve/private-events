cc41eb657d4a5b73b1034616b522c68ffeb8afb8
kossySteve <kossyeze@gmail.com>
2020-10-10 17:56:07 +0100

    add list of attendees

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   has_many :attendances
   has_many :events, through: :attendances
-  
+
+  before_save :downcase_fields
   validates :name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Please enter a valid email' }
+  
+
+   def downcase_fields
+      self.name.downcase!
+   end
 
 end
