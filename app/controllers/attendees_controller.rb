50897f666704e8f25b1741d69ec49cecffd2ffd1
mimi peshy <mimipeshy@gmail.com>
2020-11-09 23:12:27 +0300

    fix rubocop errors

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,9 +1,7 @@
 class AttendeesController < ApplicationController
-    
-
-    def create
-        @event = Event.find(params[:event_id])
-        @attendee = @event.attendees.create(params[:attendee].permit(:email))
-        redirect_to event_path(@event)
-    end  
+  def create
+    @event = Event.find(params[:event_id])
+    @attendee = @event.attendees.create(params[:attendee].permit(:email))
+    redirect_to event_path(@event)
+  end
 end
--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,4 @@
 class Attendee < ApplicationRecord
-    belongs_to :event
-    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Please enter a valid email' }
+  belongs_to :event
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Please enter a valid email' }
 end
